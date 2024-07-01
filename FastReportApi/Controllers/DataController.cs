6d637b53f0c7cea42bcb219dbb55b3591c658a27
Isaac Valdez <jvaldezb@gmail.com>
2024-07-01 17:38:38 -0500

    controller obtener id placa agregado

---
+++ FastReportApi/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Utilities;
+
+namespace FastReportApi.Controllers
+{
+    [RoutePrefix("Data")]
+    public class DataController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            List<string> listaImpresoras = new List<string>();
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                listaImpresoras.Add(printer);
+            }
+
+            var payload = new
+            {
+                pid = HardwareInfo.GetSerialNumberMainboard()
+            };
+
+            return Ok(payload);
+        }
+    }
+}
